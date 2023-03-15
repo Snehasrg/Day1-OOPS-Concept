6116ddbd6a85f6c131b09c8a277e3c5b7044dff3
Snehasrg <sghongade642@gmail.com>
2023-03-15 16:08:38 +0530

    Class is created

---
+++ ObjectandClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ObjectandClass
+    {
+        public string Name = "Royal Enfield";
+        public string Colour = "Black";
+    }
+}
