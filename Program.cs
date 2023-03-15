904c9d5fb2e30f9abb69e41420d43cb695d82278
Snehasrg <sghongade642@gmail.com>
2023-03-15 18:07:36 +0530

    Method calling for both

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OopsConcept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //StaticPolymorphism obj= new StaticPolymorphism();
+            //obj.Mult(4,5);
+            //obj.Mult(6,7.5f,7);
+            //obj.Mult(3.3f, 6.6f);
+
+            Car obj2 = new Car();
+            obj2.CarSound();
+            obj2 = new Hyundai();
+            obj2.CarSound();
+            obj2= new BMW();
+            obj2.CarSound();
+
+        }
+    }
+}
