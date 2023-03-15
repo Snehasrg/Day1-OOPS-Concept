21c1dd2f2118a5ed1fb48261ed7372e5fa86bd15
Snehasrg <sghongade642@gmail.com>
2023-03-15 17:12:31 +0530

    Inheritance Example

---
+++ Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class Inheritance
+    {
+
+        public string brand = "Hyundai";  
+        public void honk()             
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Inheritance  
+    {
+        public string modelName = "Santro"; 
+    }
+
+}
