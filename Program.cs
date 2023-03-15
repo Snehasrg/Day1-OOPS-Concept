ad586798d825055391cfc5dae44e6e3d29f51ad1
Snehasrg <sghongade642@gmail.com>
2023-03-15 18:50:50 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OopsConcept
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encapsulation encapsulation= new Encapsulation();
+            Console.WriteLine("Enter employee name");
+            encapsulation.Name = Console.ReadLine();
+            encapsulation.SetSalary(100000);
+            Console.WriteLine("Salary of "+encapsulation.Name+ " is " +encapsulation.Salary);
+        }
+    }
+}
