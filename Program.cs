4aeac48f54985edcf2c7dda06fe7e9765b054209
Snehasrg <sghongade642@gmail.com>
2023-03-15 17:14:22 +0530

    Created object for Inheritance

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿namespace OopsConcept
+﻿using System.Diagnostics.Tracing;
+
+namespace OopsConcept
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
-            ObjectandClass obj= new ObjectandClass();
-            Console.WriteLine(obj.Name);
-            Console.WriteLine(obj.Colour);
+          //Created object for Class and object Problem                  
+            //ObjectandClass obj= new ObjectandClass();
+            //Console.WriteLine(obj.Name);
+            //Console.WriteLine(obj.Colour);
+
+         // Created object for inheritance
+            Car myCar = new Car();
+            myCar.honk();
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
         }
     }
 }
