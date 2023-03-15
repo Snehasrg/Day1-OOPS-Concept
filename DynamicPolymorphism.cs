a03eb8859d21bf9d75160fc0529a4e0e5e4c333c
Snehasrg <sghongade642@gmail.com>
2023-03-15 18:05:48 +0530

    Method overriding is done

---
+++ DynamicPolymorphism.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class Car
+    {
+
+        public virtual void CarSound()
+        {
+            Console.WriteLine("The Car sound tuu tuuu!");
+        }
+    }
+
+    class Hyundai : Car  // Derived class (child) 
+    {
+        public override void CarSound()
+        {
+            Console.WriteLine("The Hyundai Sounds Vrumm Vrumm!");
+        }
+    }
+
+    class BMW : Car  // Derived class (child) 
+    {
+        public override void CarSound()
+        {
+            Console.WriteLine("The BMW Sounds Broom Broom!");
+        }
+    }
+}
