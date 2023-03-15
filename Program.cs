8ffad65b9e4977fe101344f8912c69ab5e63ad6e
Snehasrg <sghongade642@gmail.com>
2023-03-15 19:06:43 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OopsConcept
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Cow cow = new Cow();
+            cow.Eat();
+            cow.AnimalSound();
+        }
+    }
+}
