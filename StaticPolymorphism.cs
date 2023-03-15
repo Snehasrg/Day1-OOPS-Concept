51d3c71545852d4c33767d68f67657114c90d583
Snehasrg <sghongade642@gmail.com>
2023-03-15 18:05:02 +0530

    Method overloading is done

---
+++ StaticPolymorphism.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class StaticPolymorphism
+    {
+        public void Mult(int a, int b)
+        {
+            Console.WriteLine("The multiplication of two number is " + a*b);
+        }
+    
+        public void Mult(int a,float b,int c)
+        {
+            Console.WriteLine("The multiplication of three number is " + a*b*c);
+        }
+   
+        public void Mult(float a,float b)
+        {
+            Console.WriteLine("The multiplication of two number is " + a*b);
+        }
+    }
+}
