89e2155099049660f1d73d1bc1a2f227acc0a9da
Snehasrg <sghongade642@gmail.com>
2023-03-16 07:40:04 +0530

    To know different variable types

---
+++ VariableTypes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class VariableTypes
+    {
+        public static void Get()
+
+        {
+            // Integer type
+            int myNum = 5;
+            Console.WriteLine(myNum);
+            //Doubletype
+            double myDoubleNum = 5.99D;
+            Console.WriteLine(myDoubleNum);
+            //Character type
+            char myChar = 'D';
+            Console.WriteLine(myChar);
+            //Boolean type
+            bool myBool = true;
+            Console.WriteLine(myBool);
+            //String type
+            string myString = "Hello";
+            Console.WriteLine(myString);
+        }
+       
+    }
+}
