f26bd5c5f20e80db7ae139a90357f6dadf07d35a
Snehasrg <sghongade642@gmail.com>
2023-03-17 17:07:33 +0530

    Added types of inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,37 @@
+﻿using OopsConcept;
+
+namespace OopsConcept
+{
+    internal class Inheritance
+    {
+        public void honk()
+        {
+            Console.WriteLine("Hyundai");
+        }
+    }
+    //Single Inheritance
+    class Car : Inheritance
+    {
+        public void honk2()
+        {
+            Console.WriteLine("Santro");
+        }      
+    }
+    //Hierarchical Inheritance
+    class Bike:Inheritance
+    {
+        public void honk3()
+        {
+            Console.WriteLine("Royal Enfield");
+        }
+    }
+    //Multilevel Inheritance
+    class Car2:Car
+    {
+        public void honk4()
+        {
+            Console.WriteLine("Aura");
+        }
+    }
+}
+
